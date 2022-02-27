860dc5e6a5e6234caa15f974a66dc03d320066fa
Lyb-code <lyb62051169@163.com>
2022-02-27 21:23:00 +0800

    Fix shadow Acne

--- main.cc
+++ main.cc
@@ -10,7 +10,7 @@ color ray_color(const ray& r, const hittable& world, int depth) {
     if (depth <= 0) 
         return color(0, 0, 0);
 
-    if (world.hit(r, 0, inifinity, rec)) {
+    if (world.hit(r, 0.001, inifinity, rec)) {
         point3 target = rec.p + rec.normal + random_in_unit_sphere();
         return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
     }
