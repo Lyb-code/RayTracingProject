3bb9089cc84b2b50712a35a8b373acd390b4e109
Lyb-code <lyb62051169@163.com>
2022-02-27 16:40:11 +0800

    gama correction

--- color.h
+++ color.h
@@ -9,11 +9,11 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     double g = pixel_color.y();
     double b = pixel_color.z();
 
-    // divide the color by the number of samples
+    // divide the color by the number of samples and gama-correct for gama = 2.0
     auto scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = sqrt(r*scale);
+    g = sqrt(g*scale);
+    b = sqrt(b*scale);
 
     //write the translated [0, 255] value of each color component(rgb).
     out << static_cast<int>(256 * clamp(r, 0, 0.999)) << ' ' 
