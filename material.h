90b3d9e1de3ebe2f74a7e973dbb8cb4c42c82cae
Lyb-code <lyb62051169@163.com>
2022-02-28 10:54:23 +0800

    Dielectric material: Light is reflected when it cannot be refracted.

--- material.h
+++ material.h
@@ -53,4 +53,33 @@ class metal : public material {
         double fuzz;
 };
 
+class dielectric : public material {
+    public:
+        dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+        virtual bool scatter(
+            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+        ) const override {
+            attenuation = color(1, 1, 1); // the glass surface absorbs nothing
+            double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+
+            vec3 unit_direction = unit_direction(r_in.direction());
+            double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+            double sin_theta = sqrt(1 - cos_theta*cos_theta);
+            bool cannot_refract = refraction_ratio*sin_theta > 1.0;
+
+            vec3 scatter_direction;
+            if (cannot_refract) {
+                scatter_direction = reflect(unit_direction, rec.normal);
+            } else {
+                scatter_direction = refract(unit_direction, rec.normal, refraction_ratio);
+            }
+            scattered = ray(rec.p, scatter_direction);
+            return true;
+        }
+
+    public:
+        double ir; // Index of refraction
+};
+
 #endif
