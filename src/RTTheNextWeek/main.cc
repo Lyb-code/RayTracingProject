9da676c787e9133938e0c8394dbb6f4c5628bc5f
Lyb-code <lyb62051169@163.com>
2022-03-11 14:22:34 +0800

    Image Texture Mapping

--- src/RTTheNextWeek/main.cc
+++ src/RTTheNextWeek/main.cc
@@ -98,6 +98,14 @@ hittable_list two_perlin_spheres() {
     return objects;
 }
 
+hittable_list earth() {
+    auto earth_texture = make_shared<image_texture>("../../images/TheNextWeek/earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+
+    return hittable_list(globe);
+}
+
 int main() {
     // Image
     const double aspect_ratio = 16.0 / 9.0;
@@ -128,10 +136,15 @@ int main() {
             lookat = point3(0,0,0);
             vfov = 20.0;
             break;
-
-        default:
         case 3:
             world = two_perlin_spheres();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            break;
+        default:
+        case 4:
+            world = earth();
             lookfrom = point3(13,2,3);
             lookat = point3(0,0,0);
             vfov = 20.0;
