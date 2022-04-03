22fc247e687dc4c29210261746685390a3662038
Lyb-code <lyb62051169@163.com>
2022-04-03 20:30:15 +0800

    Integrating x2, importance sampling

--- src/RTTheRestOfLife/integrate_x_sq.cc
+++ src/RTTheRestOfLife/integrate_x_sq.cc
@@ -5,14 +5,14 @@
 #include <stdlib.h>
 
 inline double pdf(double x) {
-    return 0.5*x;
+    return 3*x*x / 8;
 }
 
 int main() {
-    int N = 1000000;
+    int N = 1;
     auto sum = 0.0;
     for (int i = 0; i < N; i++) {
-        auto x = sqrt(random_double(0, 4));
+        auto x = pow(random_double(0, 8), 1./3.);
         if (x != 0)sum += x*x / pdf(x);
     }
     std::cout << std::fixed << std::setprecision(12);
