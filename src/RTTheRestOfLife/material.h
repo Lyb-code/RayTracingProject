0b98f93d2af5b55886f2595ecb54df87ed57ed3d
Lyb-code <lyb62051169@163.com>
2022-04-03 21:34:12 +0800

    random hemisphere sampling

--- src/RTTheRestOfLife/material.h
+++ src/RTTheRestOfLife/material.h
@@ -9,8 +9,16 @@ struct hit_record;
 class material {
     public:
         virtual bool scatter(
-            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
-        ) const = 0;
+            const ray& r_in, const hit_record& rec, color& albedo, ray& scattered, double& pdf
+        ) const {
+            return false;
+        }
+
+        virtual double scattering_pdf(
+            const ray& r_in, const hit_record& rec, const ray& scattered
+        ) const {
+            return 0;
+        }
 
         virtual color emitted(double u, double v, const point3& p) const {
             return color(0, 0, 0);
@@ -23,78 +31,81 @@ class lambertian : public material {
         lambertian(shared_ptr<texture> a) : albedo(a) {}
         
         virtual bool scatter(
-            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+            const ray& r_in, const hit_record& rec, color& alb, ray& scattered, double& pdf
         ) const override {
-            auto scatter_direction = rec.normal + random_unit_vector();
+            auto scatter_direction = random_in_hemisphere(rec.normal);
 
-            // Catch degenerate scatter direction
-            if (scatter_direction.near_zero()) {
-                scatter_direction = rec.normal;
-            }
-
-            scattered = ray(rec.p, scatter_direction, r_in.time());
-            attenuation = albedo->value(rec.u, rec.v, rec.p);
+            scattered = ray(rec.p, unit_vector(scatter_direction), r_in.time());
+            alb = albedo->value(rec.u, rec.v, rec.p);
+            pdf = 0.5 / pi;
             return true;
+        }
+
+        double scattering_pdf(
+            const ray& r_in, const hit_record& rec, const ray& scattered
+        ) const {
+            auto cosine = dot(rec.normal, unit_vector(scattered.direction()));
+            return cosine < 0 ? 0 : cosine / pi;
         }
 
     public:
         shared_ptr<texture> albedo;
 };
 
-class metal : public material {
-    public:
-        metal(const color& a, double f): albedo(a), fuzz(f < 1 ? f : 1) {}
+// class metal : public material {
+//     public:
+//         metal(const color& a, double f): albedo(a), fuzz(f < 1 ? f : 1) {}
         
-        virtual bool scatter(
-            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
-        ) const override {
-            vec3 reflected_dir = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected_dir + fuzz * random_in_unit_sphere(), r_in.time());
-            attenuation = albedo;
-            return (dot(scattered.direction(), rec.normal) > 0);
-        }
+//         virtual bool scatter(
+//             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+//         ) const override {
+//             vec3 reflected_dir = reflect(unit_vector(r_in.direction()), rec.normal);
+//             scattered = ray(rec.p, reflected_dir + fuzz * random_in_unit_sphere(), r_in.time());
+//             attenuation = albedo;
+//             return (dot(scattered.direction(), rec.normal) > 0);
+//         }
 
-    public:
-        color albedo;
-        double fuzz;
-};
+//     public:
+//         color albedo;
+//         double fuzz;
+// };
 
-class dielectric : public material {
-    public:
-        dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+// class dielectric : public material {
+//     public:
+//         dielectric(double index_of_refraction) : ir(index_of_refraction) {}
 
-        virtual bool scatter(
-            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
-        ) const override {
-            attenuation = color(1, 1, 1); // the glass surface absorbs nothing
-            double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+//         virtual bool scatter(
+//             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+//         ) const override {
+//             attenuation = color(1, 1, 1); // the glass surface absorbs nothing
+//             double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
 
-            vec3 unit_direction = unit_vector(r_in.direction());
-            double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
-            double sin_theta = sqrt(1 - cos_theta*cos_theta);
-            bool cannot_refract = refraction_ratio*sin_theta > 1.0;
+//             vec3 unit_direction = unit_vector(r_in.direction());
+//             double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+//             double sin_theta = sqrt(1 - cos_theta*cos_theta);
+//             bool cannot_refract = refraction_ratio*sin_theta > 1.0;
 
-            vec3 scatter_direction;
-            if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double()) {
-                scatter_direction = reflect(unit_direction, rec.normal);
-            } else {
-                scatter_direction = refract(unit_direction, rec.normal, refraction_ratio);
-            }
-            scattered = ray(rec.p, scatter_direction, r_in.time());
-            return true;
-        }
+//             vec3 scatter_direction;
+//             if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double()) {
+//                 scatter_direction = reflect(unit_direction, rec.normal);
+//             } else {
+//                 scatter_direction = refract(unit_direction, rec.normal, refraction_ratio);
+//             }
+//             scattered = ray(rec.p, scatter_direction, r_in.time());
+//             return true;
+//         }
 
-    public:
-        double ir; // Index of refraction
+//     public:
+//         double ir; // Index of refraction
 
-    private:
-        static double reflectance(double cosine, double ref_idx) {
-            // Use Schlick's approximation for relectance.
-            auto r0 = (1-ref_idx) / (1+ref_idx);
-            r0 = r0*r0;
-            return r0 + (1-r0)*pow((1 - cosine),5);
-        }
-};
+//     private:
+//         static double reflectance(double cosine, double ref_idx) {
+//             // Use Schlick's approximation for relectance.
+//             auto r0 = (1-ref_idx) / (1+ref_idx);
+//             r0 = r0*r0;
+//             return r0 + (1-r0)*pow((1 - cosine),5);
+//         }
+// };
 
 class diffuse_light : public material {
     public:
@@ -102,7 +113,7 @@ class diffuse_light : public material {
         diffuse_light(color c) : diffuse_light(make_shared<solid_color>(c)) {}
 
         virtual bool scatter(
-            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered, double& pdf
         ) const override {
             return false;
         }
@@ -121,7 +132,7 @@ class isotropic : public material {
         isotropic(shared_ptr<texture> a) : albedo(a) {}
 
         virtual bool scatter(
-            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered, double &pdf
         ) const override {
             scattered = ray(rec.p, random_in_unit_sphere(), r_in.time());
             attenuation = albedo->value(rec.u, rec.v, rec.p);
