aea1d6182850b86821b9bd1c695ef2541ddf68af
Lyb-code <lyb62051169@163.com>
2022-04-05 21:33:14 +0800

    NaN-tolerant write_color function

--- src/common/color.h
+++ src/common/color.h
@@ -9,6 +9,11 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     double g = pixel_color.y();
     double b = pixel_color.z();
 
+    // Replace NaN components with zero. NaN does not equal itself;
+    if (r != r) r = 0.0;
+    if (g != g) g = 0.0;
+    if (b != b) b = 0.0; 
+
     // divide the color by the number of samples and gama-correct for gama = 2.0
     auto scale = 1.0 / samples_per_pixel;
     r = sqrt(r*scale);
